c9b28bf7ddb9be2750895f495730ab737ae3d59b
Takeshi Kakeda <takeshi@giantech.jp>
2025-08-29 09:20:53 +0900

    アルバム情報も書き込むようにした

--- lib/photozousan/client.rb
+++ lib/photozousan/client.rb
@@ -6,25 +6,73 @@ require 'fileutils'
 module Photozousan
   class Client
     PHOTO_INFO_URL = "https://api.photozou.jp/rest/photo_info.json?private=true&photo_id="
+    PHOTO_ALBUM_URL = "https://api.photozou.jp/rest/photo_album.json"
     PHOTO_ALBUM_PHOTO_URL = "https://api.photozou.jp/rest/photo_album_photo.json"
 
     def initialize(id, pass, album_id = nil)
       @certs = [id, pass]
       @album_id = album_id
+      create_album_dir(album_id) if album_id
+    end
+
+    def create_album_dir(album_id)
       timestamp = Time.now.strftime('%Y%m%d%H%M%S')
-      @base_dir = if @album_id
-                    "Original_#{@album_id}_#{timestamp}"
+      @base_dir = if album_id
+                    "Original_#{album_id}_#{timestamp}"
                   else
                     "Original_#{timestamp}"
                   end
       FileUtils.mkdir_p(@base_dir)
+
+      info = get_album_info(album_id)
+      write_album_info(info, @base_dir)
     end
 
+
     def dowmload_all_images(album_id, limit)
+      # フォルダがまだ作成されていない場合は作成
+      create_album_dir(album_id) unless @base_dir
       download(get_all_photos(album_id, limit))
     end
 
     private
+
+    def write_album_info(album_info, album_dir)
+      File.open(File.join(album_dir, 'album_info.json'), 'w') do |file|
+        file.write(album_info.to_json)
+      end
+    end
+
+    def get_album_info(album_id)
+      album_info_uri = URI.parse(PHOTO_ALBUM_URL)
+
+      response = JSON.parse(URI.open(album_info_uri,
+        http_basic_authentication: @certs,
+        ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
+      ).read)
+
+      # APIレスポンスの構造に応じて処理
+      if response.is_a?(Hash) && response['stat'] == 'ok'
+        # 成功レスポンスの場合
+        if response['info'] && response['info']['album']
+          # 指定されたalbum_idのアルバム情報を検索
+          album_info = response['info']['album'].find { |album| album['album_id'] == album_id.to_i }
+          if album_info
+            puts "Found album: #{album_info['name']} - #{album_info['description']}"
+            return album_info
+          else
+            puts "Album #{album_id} not found in the list"
+            return { 'album_id' => album_id, 'status' => 'not_found' }
+          end
+        else
+          puts "No album information in response"
+          return { 'album_id' => album_id, 'status' => 'no_album_info' }
+        end
+      else
+        puts "API call failed: #{response}"
+        return nil
+      end
+    end
 
     def get_original_image_uri(photo_id)
       extInfo_uri = URI.parse(PHOTO_INFO_URL + photo_id.to_s)
